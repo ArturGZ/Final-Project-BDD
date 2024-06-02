3a21c2d3f2e38163529967b2aeb427bed8ae9ff4
Miguel Arroyo <miguelarroyo217@gmail.com>
2024-06-02 16:05:27 -0600

    Add scripts for users creation

---
+++ s-01-autos-main-usuario.sql
@@ -0,0 +1,27 @@
+--@Autor:	        J. Miguel Arroyo Quiroz, Arturo Perez Quintana
+--@Fecha Creación:	02/06/2024
+--@Descripción:     Creación de Usuario en los 4 Nodos
+
+whenever sqlerror exit rollback;
+set serveroutput on
+
+Prompt Inciando creacion/eliminacion de usuarios
+accept syspass char prompt 'Proporcione el password de sys: ' hide
+
+Prompt Creando usuario en maqbdd_s1
+connect sys/&&syspass@maqbdd_s1 as sysdba
+@s-01-autos-usuario.sql
+
+Prompt Creando usuario en maqbdd_s2
+connect sys/&&syspass@maqbdd_s2 as sysdba
+@s-01-autos-usuario.sql
+
+Prompt Creando usuario en apqbdd_s1
+connect sys/&&syspass@apqbdd_s1 as sysdba
+@s-01-autos-usuario.sql
+
+Prompt Creando usuario en apqbdd_s1
+connect sys/&&syspass@apqbdd_s1 as sysdba
+@s-01-autos-usuario.sql
+
+Prompt Listo!!
---
+++ s-01-autos-usuario.sql
@@ -0,0 +1,23 @@
+--@Autor:	        J. Miguel Arroyo Quiroz, Arturo Perez Quintana
+--@Fecha Creación:	02/06/2024
+--@Descripción:     Eliminación y Creación del Usuario Para PF BDD
+
+Prompt Verificando la existencia del usuario autos_bdd
+declare
+    v_count number;
+    v_username varchar2(20) := 'AUTOS_BDD';
+begin
+    select count(*) into v_count from all_users where username=v_username;
+    if v_count >0 then
+        execute immediate 'drop user '||v_username||' cascade';
+    else
+        dbms_output.put_line('El usuario '||v_username||' no existe');
+    end if;
+end;
+/
+
+Prompt Creando el usuario autos_bdd
+create user autos_bdd identified by autos_bdd quota unlimited on users;
+
+Prompt Otorgando permisos al usuario autos_bdd
+grant create session, create table, create sequence, create procedure, create view, create synonym, create type, create database link to autos_bdd;
